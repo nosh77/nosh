686a682debf1fd5e307ca6a784cf6082624c4d84
nosh77 <41233345+nosh77@users.noreply.github.com>
2018-07-14 14:14:12 +0200

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://localhost/mean_db');
+var connection = mongoose.connect('mongodb://nosh:nosh77@ds137651.mlab.com:37651/nosh');
  
 module.exports = connection;
